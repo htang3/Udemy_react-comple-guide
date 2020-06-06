684f485c2b98372c944bbd40696b343578fb8ab3
Hung Q Tang <hung@neuroleap.com>
2020-06-06 14:24:39 -0700

    uses interceptor index.js, setup base url

--- http_session/src/components/FullPost/FullPost.js
+++ http_session/src/components/FullPost/FullPost.js
@@ -1,59 +1,59 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './FullPost.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./FullPost.css";
 
 class FullPost extends Component {
-    state = {
-        loadedPost: null
+  state = {
+    loadedPost: null,
+  };
+  componentDidUpdate() {
+    if (this.props.id) {
+      //make sure the newly loaded post is different from selectedPostid
+      //or either the loadedPost is not null
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
+        axios.get("posts/" + this.props.id).then((response) => {
+          //when we call setState in componentDidUpdate, it creates an infinite loop.
+          //therefore we want it only update when we load a new post, then how do we did that?
+          this.setState({
+            loadedPost: response.data,
+          });
+          //console.log(response)
+        });
+      }
     }
-    componentDidUpdate() {
-        if (this.props.id) {
-            //make sure the newly loaded post is different from selectedPostid
-            //or either the loadedPost is not null
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get("http://jsonplaceholder.typicode.com/posts/" + this.props.id)
-                    .then(response => {
-                        //when we call setState in componentDidUpdate, it creates an infinite loop.
-                        //therefore we want it only update when we load a new post, then how do we did that?
-                        this.setState({
-                            loadedPost: response.data
-                        })
-                        //console.log(response)
-                    })
-            }
-        }
-    }
+  }
 
-    deletePostHandler = () => {
-        axios.delete("http://jsonplaceholder.typicode.com/posts/" + this.props.id)
-            .then(response => {
-                console.log(response)
-            })
+  deletePostHandler = () => {
+    axios.delete("posts/" + this.props.id).then((response) => {
+      console.log(response);
+    });
+  };
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    //need to check whether it got the id or not
+    if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Loading...</p>;
     }
-    render() {
-        let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-        //need to check whether it got the id or not
-        if (this.props.id) {
-            post = <p style={{ textAlign: 'center' }}>Loading...</p>;
-        }
-        //check if axios completes getting the post or not
-        //as soon as we get post, state become true
-        if (this.state.loadedPost) {
-            post = (
-                <div className="FullPost">
-                    <h1>{this.state.loadedPost.title}</h1>
-                    <p>{this.state.loadedPost.body}</p>
-                    <div className="Edit">
-                        <button
-                            onClick={this.deletePostHandler}
-                            className="Delete">Delete</button>
-                    </div>
-                </div>
-
-            );
-        }
-        return post;
+    //check if axios completes getting the post or not
+    //as soon as we get post, state become true
+    if (this.state.loadedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button onClick={this.deletePostHandler} className="Delete">
+              Delete
+            </button>
+          </div>
+        </div>
+      );
     }
+    return post;
+  }
 }
 
 export default FullPost;
--- http_session/src/containers/Blog/Blog.js
+++ http_session/src/containers/Blog/Blog.js
@@ -1,74 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import axios from 'axios'
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import axios from "axios";
 class Blog extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            posts: [],
-            selectedPostId: null,
-            error: false
-        }
-    }
-    componentDidMount() {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-            .then(response => {
-                const updatedPosts = response.data.slice(0, 4)
-                const updatedPostsAuthor = updatedPosts.map(post => {
-                    return {
-                        ...post,
-                        author: "Max"
-                    }
-                })
-                this.setState({
-                    posts: updatedPostsAuthor
-                })
-                console.log(updatedPosts)
-            })
-            .catch(err => {
-                this.setState({
-                    error: true
-                })
-                console.log(err)
-            })
-    }
-    postSelectedhandler = (id) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      selectedPostId: null,
+      error: false,
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then((response) => {
+        const updatedPosts = response.data.slice(0, 4);
+        const updatedPostsAuthor = updatedPosts.map((post) => {
+          return {
+            ...post,
+            author: "Max",
+          };
+        });
         this.setState({
-            selectedPostId: id
-        })
-    }
+          posts: updatedPostsAuthor,
+        });
+        console.log(updatedPosts);
+      })
+      .catch((err) => {
+        this.setState({
+          error: true,
+        });
+        console.log(err);
+      });
+  }
+  postSelectedhandler = (id) => {
+    this.setState({
+      selectedPostId: id,
+    });
+  };
 
-    render() {
-        let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>
-        if (!this.state.error) {
-
-            posts = this.state.posts.map(post => {
-                return <Post
-                    clicked={() => this.postSelectedhandler(post.id)}
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                />
-            })
-        }
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post
+            clicked={() => this.postSelectedhandler(post.id)}
+            key={post.id}
+            title={post.title}
+            author={post.author}
+          />
         );
+      });
     }
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
